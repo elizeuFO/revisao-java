ec543589c23e340aedfdbb79314b42ddb66fa38b
Elizeu <Elizeu@TERMINAL13>
2021-08-11 21:00:12 -0300

    Correção

---
+++ src/programacao/CalcularMedia.java
@@ -0,0 +1,30 @@
+package programacao;
+
+/**
+ *
+ * @author Elizeu
+ */
+public class CalcularMedia {
+
+    public static void main(String[] args) {
+        int[] valores = new int[3];
+        valores[0] = 15;
+        valores[1] = 17;
+        valores[2] = 12;
+
+        double retorno_media = media(valores);
+        double media_arredondada = Arredondamento.arredondarValor(retorno_media);
+        System.out.println("MÉDIA = " + media_arredondada);
+    }
+
+    private static double media(int[] nums) {
+        double res_media = 0;
+        System.out.println("TAMANHO DO ARRAY = " + nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            res_media += nums[i];
+        }
+        res_media = res_media / nums.length;
+
+        return res_media;
+    }
+}
---
+++ src/programacao/ParImpar.java
@@ -0,0 +1,17 @@
+package programacao;
+
+/**
+ *
+ * @author Elizeu
+ */
+public class ParImpar {
+
+    public void funcao(int numero) {
+
+        if (numero % 2 == 0) {
+            System.out.println("Número " + numero + " é par.");
+        } else {
+            System.out.println("Número " + numero + " é ímpar.");
+        }
+    }
+}
